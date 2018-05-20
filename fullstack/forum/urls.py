a9ce18782f077e4efb5832b00c88068079b236ec
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-05-20 15:18:27 +0100

    create view for new_thread

--- fullstack/forum/urls.py
+++ fullstack/forum/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.forum, name='forum'),
     url(r'^threads/(?P<subject_id>\d+)/$', views.threads, name='threads'),
+    url(r'^new_thread/(?P<subject_id>\d+)/$', views.new_thread, name='new_thread'),
 ]
--- fullstack/forum/views.py
+++ fullstack/forum/views.py
@@ -2,6 +2,11 @@
 from __future__ import unicode_literals
 from django.shortcuts import render, redirect, get_object_or_404
 from models import Subject, Thread, Post
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.template.context_processors import csrf
+from .forms import ThreadForm, PostForm
 
 
 # Create your views here.
@@ -14,3 +19,37 @@ def threads(request, subject_id):
     return render(request, 'forum/threads.html', {'subject': subject})
 
 
+@login_required
+def new_thread(request, subject_id):
+    subject = get_object_or_404(Subject, pk=subject_id)
+    if request.method == "POST":
+        thread_form = ThreadForm(request.POST)
+        post_form = PostForm(request.POST)
+        if thread_form.is_valid() and post_form.is_valid():
+            thread = thread_form.save(False)
+            thread.subject = subject
+            thread.user = request.user
+            thread.save()
+
+            post = post_form.save(False)
+            post.user = request.user
+            post.thread = thread
+            post.save()
+
+            messages.success(request, "You have created a new thread!")
+
+            return redirect(reverse('thread', args=[thread.pk]))
+
+    else:
+        thread_form = ThreadForm()
+        post_form = PostForm()
+
+    args = {
+        'thread_form': thread_form,
+        'post_form': post_form,
+        'subject': subject,
+    }
+
+    args.update(csrf(request))
+
+    return render(request, 'forum/thread_form.html', args)
