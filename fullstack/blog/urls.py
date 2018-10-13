be336e9de635430da6c133e34c65e921ca080aec
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-10-13 15:08:24 +0100

    fixed 404 error when user clicks on blog item from 'Top Posts'

--- fullstack/blog/urls.py
+++ fullstack/blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='blog'),
     url(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^top/(?P<id>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^top/$', views.top_posts, name='top_posts'),
     url(r'^post/new/$', views.new_post, name='new_post'),
     url(r'^(?P<id>\d+)/edit$', views.edit_post, name='edit'),
