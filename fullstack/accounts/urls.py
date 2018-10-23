c732b8b5033861e34aa7522b3ef5780928ea0594
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-10-23 19:44:43 +0100

    add new url pattern to 'accounts/urls' to include 'urls_reset'

--- fullstack/accounts/urls.py
+++ fullstack/accounts/urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
+from . import urls_reset
 
 urlpatterns = [
     url(r'^register/$', views.register, name='register'),
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^login/$', views.login, name='login'),
     url(r'^logout/$', views.logout, name='logout'),
+    url(r'^password-reset/', include(urls_reset))
 ]
