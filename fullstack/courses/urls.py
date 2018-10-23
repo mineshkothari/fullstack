9519e01b670b38158025acb09f152df4b8cbb7fc
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-10-23 11:55:10 +0100

    add view for 'frameworks.html' and configure URLS

--- fullstack/courses/urls.py
+++ fullstack/courses/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.courses, name='courses'),
+    # url(r'^frameworks/<framework_title>/$', views.frameworks, name='frameworks'),
+    url(r'^frameworks/(?P<framework_id>\d+)/$', views.frameworks, name='frameworks'),
 ]
--- fullstack/courses/views.py
+++ fullstack/courses/views.py
@@ -11,3 +11,19 @@ from django.template.context_processors import csrf
 # Create your views here.
 def courses(request):
     return render(request, 'courses/courses.html', {'frameworks': Framework.objects.all()})
+
+
+# def frameworks(request, framework_title):
+#     framework_ = get_object_or_404(Framework, title=framework_title)
+#     return render(request, 'courses/frameworks.html', {'framework': framework_})
+
+def frameworks(request, framework_id):
+    framework_ = get_object_or_404(Framework, pk=framework_id)
+    return render(request, 'courses/frameworks.html', {'framework': framework_})
+
+
+# def language(request, language_id):
+#     language_ = get_object_or_404(Language, pk=language_id)
+#     args = {'language': language_}
+#     args.update(csrf(request))
+#     return render(request, 'courses/language.html', args)
