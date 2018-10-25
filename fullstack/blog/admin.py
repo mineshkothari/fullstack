8153520c722a998f13b51f1528245158002347e6
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-10-25 16:05:46 +0100

    update blog/forms to display django-summernote wysiwyg

--- fullstack/blog/admin.py
+++ fullstack/blog/admin.py
@@ -2,6 +2,14 @@
 from __future__ import unicode_literals
 from django.contrib import admin
 from . models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
 
-admin.site.register(Post)
+# Apply summernote to all TextField in model.
+class SomeModelAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+    summernote_fields = '__all__'
+
+
+# admin.site.register(Post)
+admin.site.register(Post, SomeModelAdmin)
+
--- fullstack/blog/forms.py
+++ fullstack/blog/forms.py
@@ -1,9 +1,13 @@
 from django import forms
 from .models import Post
+from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
 
 
 class BlogPostForm(forms.ModelForm):
 
     class Meta:
         model = Post
+        widgets = {
+            'content': SummernoteWidget(),
+        }
         fields = ('title', 'content', 'tag', 'image')
