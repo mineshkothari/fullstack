085815dbf2ac553b81480732ff6c67c5ab856aba
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-10-25 15:35:01 +0100

    added summernote to urls.py and ran migrations

--- fullstack/fullstack/urls.py
+++ fullstack/fullstack/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import url, include
+from django.conf.urls import url, include, static
 from django.contrib import admin
 from django.views.static import serve
 from settings import MEDIA_ROOT
@@ -44,9 +44,14 @@ urlpatterns = [
 
     # MEDIA
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
+
+    # DJANGO-SUMMERNOTE
+    url(r'^summernote/', include('django_summernote.urls')),
 ]
 
 
 if settings.DEBUG:
     import debug_toolbar
+    from django.conf.urls.static import static
     urlpatterns.append(url(r'^debug/', include(debug_toolbar.urls)))
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
