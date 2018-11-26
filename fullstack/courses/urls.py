57d4ada441f8d66d77fd36bc450115f967baf36f
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-11-26 12:40:52 +0000

    add page title inheritence for Courses app

--- fullstack/courses/urls.py
+++ fullstack/courses/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.courses, name='courses'),
-    url(r'^modules/(?P<language_id>\d+)/$', views.modules, name='modules'),
+    url(r'^language/(?P<language_id>\d+)/$', views.modules, name='modules'),
     url(r'^unit/(?P<module_id>\d+)/$', views.module_item, name='module_item'),
     url(r'^new-module/$', views.new_module, name='new_module'),
     url(r'^new-language/$', views.new_language, name='new_language'),
