9ba1f306ec914464245ae8f53b45e1407b80eb67
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-11-27 10:08:40 +0000

    create method 'cart_contents' on cart app

---
+++ fullstack/cart/contexts.py
@@ -0,0 +1,19 @@
+from django.shortcuts import get_object_or_404
+from courses.models import Module
+
+
+def cart_contents(request):
+    """
+    Ensure cart contents are available when rendering each page
+    """
+
+    cart = request.session.get('cart', {})
+
+    cart_items = []
+    total = 0
+    for id in cart.items():
+        module = get_object_or_404(Module, pk=id)
+        total += module.price
+        cart_items.append({'id': id, 'module': module})
+
+    return {'cart_items': cart_items, 'total': total}
