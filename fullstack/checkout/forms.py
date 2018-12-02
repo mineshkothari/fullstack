8556523e6182756f7b917d650492a8cb8802dbcc
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-12-02 00:08:32 +0000

    create checkout payment form

---
+++ fullstack/checkout/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Order
+
+
+class MakePaymentForm(forms.Form):
+
+    MONTH_CHOICES = [(i, i) for i in range(1, 13)]
+    YEAR_CHOICES = [(i, i) for i in range(2018, 2040)]
+
+    # USE 'required=False' TO PREVENT PLAIN TEXT BEING TRANSMITTED THROUGH BROWSER
+    card_number = forms.CharField(label='Card Number', required=False)
+    cvv = forms.CharField(label='CVV', required=False)
+    expiry_month = forms.ChoiceField(label="Expiry Month", choices=MONTH_CHOICES, required=False)
+    expiry_year = forms.ChoiceField(label="Expiry Year", choices=YEAR_CHOICES, required=False)
+    stripe_id = forms.CharField(widget=forms.HiddenInput)
