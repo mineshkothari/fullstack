062df20df0fce4bd1dcb110d4dc927eb8e097178
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-12-04 20:09:25 +0000

    add order form

--- fullstack/checkout/forms.py
+++ fullstack/checkout/forms.py
@@ -13,3 +13,13 @@ class MakePaymentForm(forms.Form):
     expiry_month = forms.ChoiceField(label="Expiry Month", choices=MONTH_CHOICES, required=False)
     expiry_year = forms.ChoiceField(label="Expiry Year", choices=YEAR_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = (
+            'user',
+            'is_ordered',
+            'date_ordered'
+        )
