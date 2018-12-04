ca994750f4deaee6cc7e5c9525ad2633b6084fd2
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-12-04 20:49:32 +0000

    add order_form to checkout views

--- fullstack/checkout/views.py
+++ fullstack/checkout/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.contrib import messages
 from django.utils import timezone
-from .forms import MakePaymentForm
+from .forms import MakePaymentForm, OrderForm
 from .models import Order, OrderItem
 from courses.models import Module
 import stripe
@@ -16,9 +16,13 @@ stripe.api_key = settings.STRIPE_SECRET
 @login_required(login_url='/account/login/')
 def checkout(request):
     if request.method == 'POST':
+        order_form = OrderForm(request.POST)    # NON-EDITABLE FORM (ONLY TO CAPTURE USER DATA)??
         payment_form = MakePaymentForm(request.POST)
 
-        if payment_form.is_valid():
+        if order_form.is_valid() and payment_form.is_valid():
+            order = order_form.save(commit=False)
+            order.date_ordered = timezone.now()
+            order.save()
 
             cart = request.session.get('cart', {})
             total = 0
@@ -26,7 +30,6 @@ def checkout(request):
             for module_id in cart.keys():
                 module = get_object_or_404(Module, pk=module_id)
                 total += module.price
-                order = Order
                 order_item = OrderItem(
                     order=order,
                     module=module,
@@ -55,11 +58,13 @@ def checkout(request):
             messages.error(request, 'We were unable to take a payment with that card!')
 
     else:
+        order_form = OrderForm()
         payment_form = MakePaymentForm()
 
     args = {
+        'order_form': order_form,
         'payment_form': payment_form,
         'publishable': settings.STRIPE_PUBLISHABLE,
     }
 
-    return render(request, "checkout.html", args)
+    return render(request, "checkout/checkout.html", args)
