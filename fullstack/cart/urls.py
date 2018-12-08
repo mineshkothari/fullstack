09b984d9d4746464e3c17c8f39e10cdb39bd7883
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-12-08 13:50:26 +0000

    attempt option to delete items from cart - not working

--- fullstack/cart/urls.py
+++ fullstack/cart/urls.py
@@ -5,4 +5,5 @@ from .views import view_cart, add_to_cart
 urlpatterns = [
     url(r'^$', view_cart, name='view_cart'),
     url(r'^add/(?P<module_id>\d+)$', add_to_cart, name='add_to_cart'),
+    # url(r'^delete/(?P<module_id>\d+)$', remove_from_cart, name='remove_from_cart'),
 ]
--- fullstack/cart/views.py
+++ fullstack/cart/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render, redirect, reverse
-from courses.models import Module
+from django.contrib import messages
 
 
 def view_cart(request):
@@ -23,3 +23,21 @@ def add_to_cart(request, module_id):
     request.session['cart'] = cart
 
     return render(request, "cart/cart.html")
+
+
+# def remove_from_cart(request, module_id):
+#     """
+#     Delete item from cart
+#     """
+#     cart = request.session.get('cart', {})
+#     if module_id in cart.keys():
+#         cart[module_id] = module_id
+#
+#     request.session['cart'] = cart
+#
+#     item_to_delete = cart.objects.filter(pk=module_id)
+#     if item_to_delete.exists():
+#         item_to_delete[0].delete()
+#         messages.info(request, "Item has been deleted")
+#
+#     return redirect(reverse('view_cart'))
