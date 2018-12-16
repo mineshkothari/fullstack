eb0610e3b7b77ebd393b1e0f722f968b61e398bc
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-12-16 13:50:39 +0000

    refine courses app to use better variables

--- fullstack/courses/views.py
+++ fullstack/courses/views.py
@@ -16,13 +16,17 @@ def courses(request):
 
 def modules(request, language_id):
     language = get_object_or_404(Language, pk=language_id)
-    module = Module.objects.filter(release_date__lte=timezone.now()
-                                   ).order_by('-release_date')
+    selected_modules = language.modules.filter(
+        release_date__lte=timezone.now()).order_by('-release_date')
+
+    if request.user.is_authenticated:
+        my_courses = list(request.user.module_set.all())
+    else:
+        my_courses = []
 
     args = {
-        'my_courses': request.user.module_set.all(),
-        'language': language,
-        'module': module
+        'my_courses': my_courses,
+        'modules': selected_modules,
     }
 
     return render(request, 'courses/modules.html', args)
@@ -41,7 +45,7 @@ def module_item(request, module_id):
 
 @login_required(login_url='/account/login/')
 def new_module(request):
-    if request.user.is_authenticated and request.user.is_staff:
+    if request.user.is_staff:
         if request.method == 'POST':
             form = NewModuleForm(request.POST)
             if form.is_valid():
@@ -51,44 +55,24 @@ def new_module(request):
                 messages.success(request, "New module successfully added")
                 return redirect(module_item, module.pk)
         else:
-            form = NewModuleForm
-            return render(request, 'courses/new_module.html', {'form': form})
+            form = NewModuleForm()
+        return render(request, 'courses/new_module.html', {'form': form})
     else:
         return redirect(reverse('courses'))
 
 
-# def new_module(request, language_id):
-#     language = get_object_or_404(Language, pk=language_id)
-#
-#     if request.user.is_authenticated and request.user.is_staff:
-#         if request.method == 'POST':
-#             form = NewModuleForm(request.POST)
-#             if form.is_valid():
-#                 module = form.save(False)
-#                 module.language = language
-#                 module.release_date = timezone.now()
-#                 module.save()
-#                 messages.success(request, "New module successfully added")
-#                 return redirect(module_item, language.pk)
-#         else:
-#             form = NewModuleForm
-#             return render(request, 'courses/new_module.html', {'form': form})
-#     else:
-#         return redirect(reverse('courses'))
-
 @login_required(login_url='/account/login/')
 def new_language(request):
-    if request.user.is_authenticated and request.user.is_staff:
+    if request.user.is_staff:
         if request.method == 'POST':
             form = NewLanguageForm(request.POST)
+            print form.data
             if form.is_valid():
-                language = form.save(False)
-                language.release_date = timezone.now()
-                language.save()
-                messages.success(request, "New language successfully created")
-                return redirect(module_item, language.pk)
+                language = form.save()
+                messages.success(request, "%s successfully created" % language.title)
+                return redirect(reverse('courses'))
         else:
-            form = NewLanguageForm
-            return render(request, 'courses/new_language.html', {'form': form})
+            form = NewLanguageForm()
+        return render(request, 'courses/new_language.html', {'form': form})
     else:
         return redirect(reverse('courses'))
