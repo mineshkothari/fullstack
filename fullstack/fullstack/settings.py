b4cf6c4bbba3543fb117fb0eade3ec80e6b5b597
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-12-18 14:18:03 +0000

    install 'django-storages' and 'boto3' / configure in settings

--- fullstack/fullstack/settings.py
+++ fullstack/fullstack/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -65,6 +65,7 @@ INSTALLED_APPS = [
     'tinymce',
     'emoticons',
     'paypal.standard.ipn',
+    'storages',
 ]
 
 INTERNAL_IPS = ('127.0.0.1',)
@@ -154,6 +155,21 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+# AWS S3 CONFIG
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000'
+}
+
+AWS_STORAGE_BUCKET_NAME = 'mk-fullstack'
+AWS_S3_REGION_NAME = 'eu-west-2',
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STATICFILES_STORAGES = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
