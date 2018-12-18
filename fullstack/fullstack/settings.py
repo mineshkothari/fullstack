ec81eebbaa241c455c13d1cb8c48b51ce8707186
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-12-18 22:51:42 +0000

    fix issue with allowed host to run from herokuapp

--- fullstack/fullstack/settings.py
+++ fullstack/fullstack/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://mk-fullstack.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'mk-fullstack.herokuapp.com']
 
 SITE_ID = 2
 
