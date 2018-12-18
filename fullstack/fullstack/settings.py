d6a365f4ae389277c02abc782aa8abff8f834149
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-12-18 22:46:39 +0000

    made env module optional

--- fullstack/fullstack/settings.py
+++ fullstack/fullstack/settings.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env
+try:
+    import env
+except ImportError:  # RELYING ON ENVIRONMENT VARIABLES
+    pass
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
