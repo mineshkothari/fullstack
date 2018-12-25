51eb44f7140193c1af4cc517ca0282c018b0afd4
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-12-25 20:09:36 +0000

    configure separate requirements for dev and staging

--- fullstack/settings/staging.py
+++ fullstack/settings/staging.py
@@ -10,17 +10,10 @@ ALLOWED_HOSTS.append('mk-fullstack.herokuapp.com')
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+# Load PostgresDB
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 # Stripe environment variables
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
