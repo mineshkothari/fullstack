0de11abb409d5711a57e78fce33c7b2878a51a7a
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-12-27 21:49:49 +0000

    add max-length to payment details for improved validation

--- fullstack/checkout/forms.py
+++ fullstack/checkout/forms.py
@@ -8,17 +8,8 @@ class MakePaymentForm(forms.Form):
     YEAR_CHOICES = [(i, i) for i in range(2018, 2040)]
 
     # USE 'required=False' TO PREVENT PLAIN TEXT BEING TRANSMITTED THROUGH BROWSER
-    card_number = forms.CharField(label='Card Number', required=False)
-    cvv = forms.CharField(label='CVV', required=False)
+    card_number = forms.CharField(label='Card Number', required=False, max_length=19)
+    cvv = forms.CharField(label='CVV', required=False, max_length=4)
     expiry_month = forms.ChoiceField(label="Expiry Month", choices=MONTH_CHOICES, required=False)
     expiry_year = forms.ChoiceField(label="Expiry Year", choices=YEAR_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
-
-
-# class OrderForm(forms.ModelForm):
-#     class Meta:
-#         model = Order
-#         fields = (
-#             'user',
-#             'is_ordered',
-#         )
