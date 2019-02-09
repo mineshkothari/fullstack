09097eb10411fcaa76d402ff26a30e7f503fa8f9
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2019-02-09 13:20:26 +0000

    add comment to blog/views and refactor 'id' to 'post_id' for clear separation

--- fullstack/blog/urls.py
+++ fullstack/blog/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='blog'),
-    url(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'),
-    url(r'^top/(?P<id>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^(?P<post_id>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^top/(?P<post_id>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^top/$', views.top_posts, name='top_posts'),
     url(r'^post/new/$', views.new_blog_post, name='new_blog_post'),
-    url(r'^(?P<id>\d+)/edit$', views.edit_post, name='edit'),
+    url(r'^(?P<post_id>\d+)/edit$', views.edit_post, name='edit'),
 ]
--- fullstack/blog/views.py
+++ fullstack/blog/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.utils import timezone
 from django.template.context_processors import csrf
 from .models import Post
@@ -8,22 +8,29 @@ from .forms import BlogPostForm
 from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
 def post_list(request):
-
+    """
+    Render blog posts on the page and order them by descending published date
+    """
     posts = Post.objects.filter(published_date__lte=timezone.now()
                                 ).order_by('-published_date')
     return render(request, 'blog/blogposts.html', {'posts': posts})
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, pk=id)
+def post_detail(request, post_id):
+    """
+    Render individual blog post articles
+    """
+    post = get_object_or_404(Post, pk=post_id)
     post.views += 1
     post.save()
     return render(request, 'blog/postdetail.html', {'post': post})
 
 
 def top_posts(request):
+    """
+    Render the published blog posts in order of popularity and limit to 5 posts
+    """
     posts = Post.objects.filter(published_date__lte=timezone.now()
                                 ).order_by('-views')[:5]
     return render(request, 'blog/blogposts.html', {'posts': posts})
@@ -31,44 +38,67 @@ def top_posts(request):
 
 @login_required(login_url='/account/login/')
 def new_blog_post(request):
-    if request.method == 'POST':
-        form = BlogPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect(post_detail, post.pk)
+    """
+    Submit or render new blog post form
+    """
+    # First check if user is staff. If True and method is POST then save blog
+    if request.user.is_staff:
+        if request.method == 'POST':
+            form = BlogPostForm(request.POST, request.FILES)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect(post_detail, post.pk)
+
+        # If method is GET, render blank from
+        else:
+            form = BlogPostForm()
+
+        args = {
+            'form': form,
+            'form_title': 'Add New Blog'
+        }
+
+        args.update(csrf(request))
+
+        return render(request, 'blog/blogpostform.html', args)
+
+    # If user does not have Staff status, redirect to blog homepage
     else:
-        form = BlogPostForm()
-
-    args = {
-        'form': form,
-        'form_title': 'Add New Blog'
-    }
-
-    args.update(csrf(request))
-
-    return render(request, 'blog/blogpostform.html', args)
+        return redirect(reverse('blog'))
 
 
 @login_required(login_url='/account/login/')
-def edit_post(request, id):
-    post = get_object_or_404(Post, pk=id)
-    if request.method == "POST":
-        form = BlogPostForm(request.POST, request.FILES, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect(post_detail, post.pk)
+def edit_post(request, post_id):
+    """
+    Submit or render edit blog post form
+    """
+    post = get_object_or_404(Post, pk=post_id)
+
+    # First check if user is staff. If True and method is POST then update blog
+    if request.user.is_staff:
+        if request.method == "POST":
+            form = BlogPostForm(request.POST, request.FILES, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect(post_detail, post.pk)
+
+        # If method is GET, render blank from
+        else:
+            form = BlogPostForm(instance=post)
+
+        args = {
+            'form': form,
+            'form_title': 'Edit ' + post.title
+        }
+
+        return render(request, 'blog/blogpostform.html', args)
+
+    # If user does not have Staff status, redirect to blog homepage
     else:
-        form = BlogPostForm(instance=post)
-
-    args = {
-        'form': form,
-        'form_title': 'Edit ' + post.title
-    }
-
-    return render(request, 'blog/blogpostform.html', args)
+        return redirect(reverse('blog'))
